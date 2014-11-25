639a23c92c40cf83e625f8803e82acabf1f661d4
wangh <303792351@qq.com>
2014-11-25 21:34:53 +0800

    201411252134

---
+++ 发布/发布.cpp
@@ -0,0 +1,55 @@
+/*
+发布
+*/
+
+#include <iostream>
+#include <deque>
+using namespace std;
+
+#include <RCF/RCF.hpp>
+#include <RCF/RemoteCallContext.hpp>
+
+RCF_BEGIN(I_HelloWorld, "I_HelloWorld")
+RCF_METHOD_R1(int, Print, const string &)
+RCF_END(I_HelloWorld)
+
+class HelloWorld
+{
+public:
+	int Print(const string &s)
+	{
+		cout << s << endl;
+		return s.size();
+	}
+};
+
+int main()
+{
+	RCF::init();
+	try
+	{
+		RCF::RcfServer pubServer(RCF::TcpEndpoint(50001));
+		pubServer.start();
+		typedef boost::shared_ptr<RCF::Publisher<I_HelloWorld> > HelloWorldPubPtr;
+		HelloWorldPubPtr pubPtr = pubServer.createPublisher<I_HelloWorld>();
+
+		while (true)
+		{
+			RCF::sleepMs(1000);
+			pubPtr->publish().Print("HelloWorld");
+		}
+
+		pubPtr->close();
+	}
+	catch (RCF::Exception &e)
+	{
+		cout << e.getErrorString() << endl;
+	}
+
+	cout << "任意键退出..." << endl;
+	cin.get();
+
+
+
+	return 0;
+}
