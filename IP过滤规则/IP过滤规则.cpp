c1f329b4234636ec4d5c84bd24332592d861dfea
wangh <303792351@qq.com>
2014-11-27 23:01:49 +0800

    201411272301

---
+++ IP过滤规则/IP过滤规则.cpp
@@ -0,0 +1,45 @@
+/*
+
+*/
+
+#include <iostream>
+using namespace std;
+
+#include <RCF/RCF.hpp>
+
+RCF_BEGIN(I_HelloWorld, "I_HelloWorld")
+	RCF_METHOD_V1(void, Print, const string &)
+RCF_END(I_HelloWorld)
+
+class HelloWorld
+{
+public:
+	void Print(const string &s)
+	{
+		cout << "服务器: " << s << endl;
+	}
+};
+
+int main()
+{
+	try{
+		RCF::init();
+
+		RCF::RcfServer server(RCF::TcpEndpoint(50001));
+		HelloWorld helloWorld;
+		server.bind<I_HelloWorld>(helloWorld);
+		server.start();
+
+		RcfClient<I_HelloWorld> client(RCF::TcpEndpoint(50001));
+		client.Print("我是客户端1");
+	}
+	catch (RCF::Exception &e)
+	{
+		cout << e.getErrorString() << endl;
+	}
+
+	cout << "任意键退出..." << endl;
+	cin.get();
+
+	return 0;
+}
