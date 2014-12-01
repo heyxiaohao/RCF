3a5d309ce4e75716e9cdaee27055abd461eb75f8
wangh <hey-xiaohao@163.com>
2014-12-01 16:38:52 +0800

    201412011638

--- Schannel/Schannel.cpp
+++ Schannel/Schannel.cpp
@@ -1,10 +1,14 @@
 /*
+RCF::PfxCertificate类  读取证书  .pfx / .p12文件
+或者从windows证书管理区读取
+RCF::StoreCertificate类
 */
 
 #include <iostream>
 using namespace std;
 
 #include <RCF/RCF.hpp>
+#include <RCF/Win32Certificate.hpp>
 
 RCF_BEGIN(I_HelloWorld, "I_HelloWorld")
 	RCF_METHOD_V1(void, Print, const string &)
@@ -24,6 +28,13 @@ int main()
 	try{
 		RCF::init();
 		RCF::RcfServer server(RCF::TcpEndpoint(50001));
+		// 读取证书文件
+		RCF::CertificatePtr serverCertPtr(new RCF::PfxCertificate(string("c:/server.p12"), _T("password"), _T("name")));
+		server.setCertificate(serverCertPtr);
+		// 从证书管理区读取
+		RCF::CertificatePtr serverCerPtr1(new RCF::StoreCertificate(RCF::Cl_LocalMachine, RCF::Cs_My, _T("name")));
+
+
 		HelloWorld helloWorld;
 		server.bind<I_HelloWorld>(helloWorld);
 		server.start();
