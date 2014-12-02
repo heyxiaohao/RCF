78a38ac4d9a612afccd5a579083977124c7d9551
wangh <hey-xiaohao@163.com>
2014-12-02 11:14:00 +0800

    201412021113

--- 发布/发布.cpp
+++ 发布/发布.cpp
@@ -1,5 +1,6 @@
 /*
-发布
+发布，可发布多个主题，唯一要求是，订阅方能发起网络连接，发布方从不与订阅方发起连接
+RcfServer::createPublisher<>();
 */
 
 #include <iostream>
@@ -11,15 +12,15 @@ RCF_BEGIN(I_HelloWorld, "I_HelloWorld")
 	RCF_METHOD_R1(int, Print, const string &)
 RCF_END(I_HelloWorld)
 
-class HelloWorld
-{
-public:
-	int Print(const string &s)
-	{
-		cout << s << endl;
-		return s.size();
-	}
-};
+//class HelloWorld
+//{
+//public:
+//	int Print(const string &s)
+//	{
+//		cout << s << endl;
+//		return s.size();
+//	}
+//};
 
 int main()
 {
@@ -29,14 +30,20 @@ int main()
 		RCF::RcfServer pubServer(RCF::TcpEndpoint(50001));
 		pubServer.start();
 		typedef boost::shared_ptr<RCF::Publisher<I_HelloWorld> > HelloWorldPubPtr;
+		// 创建默认名称的Publisher(运行时标示"I_HelloWorld"
 		HelloWorldPubPtr pubPtr = pubServer.createPublisher<I_HelloWorld>();
+		// 手动设置主题名
+		RCF::PublisherParms pubParms;
+		pubParms.setTopicName("HelloWorld_Topic_1");
+		HelloWorldPubPtr pub1Ptr = pubServer.createPublisher<I_HelloWorld>(pubParms);
 
 		while (true)
 		{
 			RCF::sleepMs(1000);
-			pubPtr->publish().Print("HelloWorld");
+			pubPtr->publish().Print("HelloWorld"); // OneWay方式
 		}
-
+		// 关闭
+		pub1Ptr->close();
 		pubPtr->close();
 	}
 	catch (RCF::Exception &e)
