b0e01e9fbda9d522ed5aef2d1d1e3f048b4475fb
wangh <hey-xiaohao@163.com>
2014-12-02 14:57:42 +0800

    201412021457

--- 服务器端/服务器端.cpp
+++ 服务器端/服务器端.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
 
 #include <RCF/RCF.hpp>
@@ -12,17 +13,39 @@ RCF_BEGIN(I_HelloWorld, "I_HelloWorld")
 	RCF_METHOD_V1(void, Print, const string &)
 RCF_END(I_HelloWorld)
 
+RCF_BEGIN(I_IdentifyClient, "I_IdentifyClient")
+	RCF_METHOD_V1(void, SetClientName, const string &)
+RCF_END(I_IdentifyClient)
 
-typedef boost::shared_ptr<RcfClient<I_HelloWorld> > HelloWorldPtr;
+// 设置客户端名字，用于识别
+class IdentifyClient
+{
+public:
+	void SetClientName(const string &clientName)
+	{
+		RCF::RcfSession &session = RCF::getCurrentRcfSession();
+		string &sessionClientName = session.createSessionObject<string>();
+		sessionClientName = clientName;
+	}
+};
+
+typedef RcfClient<I_HelloWorld> helloWorldClient;
+typedef boost::shared_ptr<helloWorldClient > HelloWorldPtr;
 RCF::Mutex gCallbackClientMutex;
-vector<HelloWorldPtr> gCallbackClients;
+//vector<HelloWorldPtr> gCallbackClients;
+map<string, HelloWorldPtr> gCallbackClients;
 
 // 回调连接创建完毕调用
 void onCallbackConnectionCreated(RCF::RcfSessionPtr sessionPtr, RCF::ClientTransportAutoPtr transportPtr)
 {
+	string &clientName = sessionPtr->getSessionObject<string>();
+	cout << "clientName" << clientName << endl;
+
+	// 保存
 	HelloWorldPtr helloWorldPtr(new RcfClient<I_HelloWorld>(transportPtr));
 	RCF::Lock lock(gCallbackClientMutex);
-	gCallbackClients.push_back(helloWorldPtr);
+	//gCallbackClients.push_back(helloWorldPtr);
+	gCallbackClients[clientName] = helloWorldPtr;
 }
 
 int main()
@@ -30,8 +53,10 @@ int main()
 	RCF::init();
 	try
 	{
-		RCF::RcfServer server((RCF::TcpEndpoint(50001)));
+		RCF::RcfServer server((RCF::TcpEndpoint(50004)));
 		server.setOnCallbackConnectionCreated(boost::bind(&onCallbackConnectionCreated, _1, _2));
+		IdentifyClient identifyClient;
+		server.bind<I_IdentifyClient>(identifyClient);
 		server.start();
 
 		// 等待连接，可单独创建等待线程
@@ -41,7 +66,8 @@ int main()
 		}
 
 		// 开始回调，此处可单独创建处理线程
-		vector<HelloWorldPtr> clients;
+		//vector<HelloWorldPtr> clients;
+		map<string, HelloWorldPtr> clients;
 		{
 			RCF::Lock lock(gCallbackClientMutex);
 			clients.swap(gCallbackClients);
@@ -49,7 +75,8 @@ int main()
 		// 调用客户端函数
 		for (size_t i = 0; i < clients.size(); ++i)
 		{
-			HelloWorldPtr client = clients[i];
+			HelloWorldPtr client = clients["wangh"];
+			//HelloWorldPtr client = clients[i];
 			client->Print("hello网");
 		}
 
@@ -59,6 +86,8 @@ int main()
 	catch (RCF::Exception &e)
 	{
 		cout << e.getErrorString() << endl;
+		cout << "任意键退出..." << endl;
+		cin.get();
 	}
 	return 0;
 }
